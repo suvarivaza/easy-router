095f32a4c9c835576f668df9604b544bfd17cb78
42-36-42@mail.ru <mWYA*&t93k^W>
2020-08-16 13:12:04 +0300

    change in class
    Signed-off-by: 42-36-42@mail.ru <mWYA*&t93k^W>

--- src/EasyRouter.php
+++ src/EasyRouter.php
@@ -32,18 +32,25 @@ class EasyRouter
 
                 $route = $routes[$uri];
 
+                // if a handler file is specified in routes
+                // check file exists and include file
                 if(isset($route['file'])){
-                    include $route['path'] . $route['file'];
-                    exit;
+                    if(file_exists ( $route['path'] . $route['file'] )){
+                        include $route['path'] . $route['file'];
+                        exit;
+                    }
                 }
 
+                // if a class or method is specified in the routes
                 $this->path = $route['path'];
                 $param = explode('/', $route['controller']);
                 $controller = $param[0];
                 if(isset($param[1])) $method = $param[1];
 
+                // if class exists create new object same class
                 if (class_exists($controller)) {
                     $controllerObject = new $controller;
+                    // if method exists call this method
                     if(isset($method)){
                         if(method_exists($controller, $method)){
                             $controllerObject->{$method}();
@@ -56,10 +63,12 @@ class EasyRouter
                     throw new \Exception("Failed to load class: $controller");
                 }
             } else {
+                // if page 404 is specified
                 if(isset($routes['/404'])){
-
-                    include $routes['/404']['path'] . $routes['/404']['file'];
-                    exit;
+                    if(file_exists ( $routes['/404']['path'] . $routes['/404']['file'] )) {
+                        include $routes['/404']['path'] . $routes['/404']['file'];
+                        exit;
+                    }
                 }
                 throw new \Exception('Page not found 404', 404);
             }
